729c25a90f698c49c1ccf4bccf6c016f72edab6f
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-01-27 19:09:07 +0300

    Add API endpoint categories/

--- diploma-backend/webshop/products/serializers.py
+++ diploma-backend/webshop/products/serializers.py
@@ -6,24 +6,40 @@ from .models import Category
 User = get_user_model()
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'title', 'parent', 'image', 'image_alt']
-        read_only_fields = ('id', 'title', 'parent', 'image', 'image_alt')
+class CategoryImageRepresentationMixin:
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        image = {
+            'src': data['image'],
+            'alt': data['image_alt'],
+        }
+        data['image'] = image
+        data.pop('image_alt')
 
-    def validate(self, data):
-        if data.get('parent') == self.instance:
-            raise serializers.ValidationError(
-                "Category cannot be parent of itself"
-            )
         return data
 
 
-class TopLevelCategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(
+    CategoryImageRepresentationMixin, serializers.ModelSerializer
+):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'image', 'image_alt']
+        read_only_fields = ('id', 'title', 'image', 'image_alt')
+
+
+class TopLevelCategorySerializer(
+    CategoryImageRepresentationMixin, serializers.ModelSerializer
+):
     subcategories = CategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Category
         fields = ['id', 'title', 'image', 'image_alt', 'subcategories']
-        read_only_fields = ('id', 'title', 'parent', 'image', 'image_alt')
+        read_only_fields = (
+            'id',
+            'title',
+            'image',
+            'image_alt',
+            'subcategories',
+        )
--- diploma-backend/webshop/products/urls.py
+++ diploma-backend/webshop/products/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+
+from .views import TopLevelCategoryListView
+
 app_name = 'products'
 
-urlpatterns = []
+urlpatterns = [
+    path('categories/', TopLevelCategoryListView.as_view(), name='categories'),
+]
