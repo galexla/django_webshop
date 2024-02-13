b645c95a059dfeaebdd3016791663e17a4d4e959
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-02-13 21:55:32 +0300

    Add textarea widget to Category & Product admin

--- diploma-backend/webshop/products/forms.py
+++ diploma-backend/webshop/products/forms.py
@@ -8,6 +8,12 @@ class CategoryAdminForm(forms.ModelForm):
         model = Category
         fields = '__all__'
 
+    image_alt = forms.CharField(
+        required=False,
+        max_length=200,
+        widget=forms.Textarea,
+    )
+
     def __init__(self, *args, **kwargs):
         super(CategoryAdminForm, self).__init__(*args, **kwargs)
         self.fields['parent'].queryset = Category.objects.filter(
@@ -19,3 +25,14 @@ class ProductAdminForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ['date']
+
+    description = forms.CharField(
+        required=False,
+        max_length=3000,
+        widget=forms.Textarea,
+    )
+    full_description = forms.CharField(
+        required=False,
+        max_length=20000,
+        widget=forms.Textarea,
+    )
