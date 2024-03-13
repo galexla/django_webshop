4ddbcea464d7229d3406263a1b33e4f48a90de98
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-03-13 17:26:16 +0300

    Forbid configuration craetion, deletion, etc
    - forbid configuration craetion & deletion
    - forbid description editing
    - add validation
    - add configuration retrieving function

--- diploma-backend/webshop/configurations/admin.py
+++ diploma-backend/webshop/configurations/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.core.exceptions import PermissionDenied
 
 from .models import ShopConfiguration
 
@@ -10,23 +9,11 @@ class ShopConfigurationAdmin(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj and obj.key in ShopConfiguration.protected_keys:
-            return ('key',)
+            return ('key', 'description')
         return ()
 
-    def delete_model(self, request, obj):
-        if obj.key in ShopConfiguration.protected_keys:
-            raise PermissionDenied(
-                f'The "{obj.key}" configuration is protected and cannot be deleted'
-            )
-        super().delete_model(request, obj)
-
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if 'delete_selected' in actions:
-            del actions['delete_selected']
-        return actions
-
     def has_delete_permission(self, request, obj=None):
-        if obj is not None and obj.key in ShopConfiguration.protected_keys:
-            return False
-        return super().has_delete_permission(request, obj=obj)
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
--- diploma-backend/webshop/configurations/models.py
+++ diploma-backend/webshop/configurations/models.py
@@ -1,6 +1,8 @@
 import logging
 
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.forms import ValidationError
 
 log = logging.getLogger(__name__)
 
@@ -11,7 +13,7 @@ class ShopConfiguration(models.Model):
     description = models.TextField(blank=True)
 
     protected_keys = [
-        'regular_delivery_price',
+        'ordinary_delivery_price',
         'express_delivery_price',
         'free_delivery_limit',
     ]
@@ -19,12 +21,28 @@ class ShopConfiguration(models.Model):
     def __str__(self):
         return self.key
 
+    def clean_value(self):
+        """
+        Validates and converts 'value'. Raises ValidationError if
+        validation fails.
+        """
+        try:
+            int_value = int(self.value)
+            MinValueValidator(0)(int_value)
+            return int_value
+        except (ValueError, ValidationError) as e:
+            raise ValidationError(f'Value must be a positive integer number')
+
+    def clean(self) -> None:
+        self.clean_value()
+        super().clean()
+
     def save(self, *args, **kwargs):
         if self.pk and self.key in self.protected_keys:
             original_key = ShopConfiguration.objects.get(pk=self.pk).key
             if self.key != original_key:
                 log.info(
-                    f'Modification attempt blocked for protected configuration: {self.key}'
+                    f'Renaming attempt blocked for protected configuration: {self.key}'
                 )
                 return
 
@@ -38,3 +56,10 @@ class ShopConfiguration(models.Model):
             return
 
         super().delete(*args, **kwargs)
+
+
+def get_shop_configuration(key: str):
+    item = ShopConfiguration.objects.filter(key=key).first()
+    if item is None:
+        return 0
+    return item.clean_value()
