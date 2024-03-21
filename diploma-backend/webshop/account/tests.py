78b27d1dfb1645693f4effa0f5b1451178fa6613
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-03-21 21:46:02 +0300

    Add tests for SetPasswordView, fix account tests

--- diploma-backend/webshop/account/tests.py
+++ diploma-backend/webshop/account/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.response import Response
 
-from .models import User
+from .models import Profile, User
 
 
 class SignInViewTest(TestCase):
@@ -12,25 +11,32 @@ class SignInViewTest(TestCase):
         cls.user1 = User.objects.create_user(
             username='user1', password='secret', email='user1@user.com'
         )
+        cls.user1.profile = Profile.objects.create(user=cls.user1)
         cls.user2 = User.objects.create_user(
             username='user2',
             password='secret',
             email='user2@user.com',
             is_active=False,
         )
+        cls.user2.profile = Profile.objects.create(user=cls.user2)
 
     @classmethod
     def tearDownClass(cls) -> None:
+        cls.user1.profile.delete()
         cls.user1.delete()
+        cls.user2.profile.delete()
         cls.user2.delete()
 
-    def test_signin(self):
+    def test_post(self):
         url = reverse('account:sign-in')
+
         response = self.client.post(
             url, {'username': 'user1', 'password': 'secret'}
         )
         self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(self.user1.is_authenticated)
+
+        response = self.client.get(reverse('account:profile'))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
 
         response = self.client.post(
             url, {'username': 'user2', 'password': 'secret'}
@@ -72,7 +78,7 @@ class SignOutViewTest(TestCase):
     def tearDownClass(cls) -> None:
         cls.user1.delete()
 
-    def test_signin(self):
+    def test_post(self):
         url = reverse('account:sign-out')
 
         response = self.client.post(url)
@@ -81,7 +87,8 @@ class SignOutViewTest(TestCase):
         self.client.force_login(self.user1)
         response = self.client.post(url)
         self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertFalse(self.user1.is_authenticated)
+        response = self.client.get(reverse('account:profile'))
+        self.assertEquals(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 class SignUpViewTest(TestCase):
@@ -94,12 +101,13 @@ class SignUpViewTest(TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         cls.user1.delete()
+        Profile.objects.filter(user__username='user2').delete()
         User.objects.filter(username='user2').delete()
 
-    def test_signin(self):
+    def test_post(self):
         url = reverse('account:sign-up')
 
-        response: Response = self.client.post(
+        response = self.client.post(
             url,
             {'username': 'user1', 'password': 'secret', 'name': 'user'},
         )
@@ -107,15 +115,21 @@ class SignUpViewTest(TestCase):
             response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
-        response: Response = self.client.post(
-            url,
-            {'username': 'user2', 'password': '123qazxsw', 'name': 'user'},
-        )
+        post_data = {
+            'username': 'user2',
+            'password': '123qazxsw',
+            'name': 'user',
+        }
+        response = self.client.post(url, post_data)
         self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(User.objects.filter(username='user2').exists())
-        self.assertTrue(User.objects.get(username='user2').is_authenticated)
+        user = User.objects.filter(username='user2').first()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, post_data['username'])
+        self.assertEqual(user.first_name, post_data['name'])
+        response = self.client.get(reverse('account:profile'))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
 
-        response: Response = self.client.post(
+        response = self.client.post(
             url,
             {'username': 'user1', 'password': 'secret', 'name12345': 'user'},
         )
@@ -123,7 +137,7 @@ class SignUpViewTest(TestCase):
             response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
-        response: Response = self.client.post(
+        response = self.client.post(
             url,
             {'username': 'user3', 'password': 'secret', 'name1': 'user'},
         )
@@ -131,7 +145,59 @@ class SignUpViewTest(TestCase):
             response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR
         )
 
-        response: Response = self.client.post(url, {})
+        response = self.client.post(url, {})
         self.assertEquals(
             response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR
         )
+
+
+class SetPasswordViewTest(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.user1 = User.objects.create_user(
+            username='user1', password='secret', email='user1@user.com'
+        )
+        cls.user1.profile = Profile.objects.create(user=cls.user1)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.user1.profile.delete()
+        cls.user1.delete()
+
+    def test_post(self):
+        url = reverse('account:password')
+
+        response = self.client.post(
+            url,
+            {'currentPassword': 'secret', 'newPassword': 'newPass321'},
+        )
+        self.assertEquals(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        self.client.force_login(self.user1)
+
+        response = self.client.post(
+            url,
+            {'currentPassword': 'secret1', 'newPassword': 'newPass321'},
+        )
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.post(
+            url,
+            {'currentPassword': 'secret', 'newPassword': 'newPass'},
+        )
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.post(
+            url,
+            {'currentPassword': 'secret', 'newPassword': 'newPass321'},
+        )
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        response = self.client.post(
+            reverse('account:sign-in'),
+            {'username': 'user1', 'password': 'newPass321'},
+        )
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        response = self.client.get(reverse('account:profile'))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        self.client.logout()
