b9d46f9a8a17a8362fe9bdfc89e32dea69338d9b
Aleksandr Gladkikh <20492198+galexla@users.noreply.github.com>
2024-05-12 18:06:21 +0300

    Add user activation/deactivation to admin panel

--- backend/webshop/account/admin.py
+++ backend/webshop/account/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from .models import User
 
 
+@admin.action(description='Activate users')
+def mark_active(
+    modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet
+):
+    queryset.update(is_active=True)
+
+
+@admin.action(description='Deactivate users')
+def mark_inactive(
+    modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet
+):
+    queryset.update(is_active=False)
+
+
 @admin.register(User)
 class CategoryAdmin(UserAdmin):
+    actions = [
+        mark_active,
+        mark_inactive,
+    ]
     list_display = [
         'username',
         'email',
